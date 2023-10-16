3521b7b428fe3766116bbc6e81f983fb5d39412e
Safvan-tsy <safvanmanikulath@gmail.com>
2023-10-16 22:44:33 +0530

    hero

---
+++ app/components/hero/Hero.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './Hero.module.css'
+import Image from 'next/image';
+import profile from './../../assets/profile.png'
+
+const Hero = () => {
+    return (
+        <div className={styles.hero}>
+                <div className={styles.heading}>
+                <h1 className={styles.heading1}>HI 👋,</h1>
+                <h1 className={styles.heading1}>I'M SAFVAN</h1>
+                <h2 className={styles.heading2}>SOFTWARE DEVELOPER</h2>
+                </div>
+                <div className={styles.image}>
+                <Image
+                    src={profile}
+                    width={300}
+                    height={300}
+                    alt="Picture of the author"
+                />
+                </div>
+        </div>
+    )
+}
+
+export default Hero
--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -14,9 +14,6 @@ const NavBar = () => {
         <>
             <nav className={styles.nav}>
                 <ul className={styles.list}>
-                    {/* <li className={styles.listItem}>
-                        <img className={styles.img} src="/favicon.ico" alt="Logo" />
-                    </li> */}
                     <li className={styles.listItem}>
                         <Link href="/page1">
                             <FaHome />
@@ -44,45 +41,6 @@ const NavBar = () => {
                     </li>
                 </ul>
             </nav>
-            {/* <nav className={styles.toggleNav}>
-                <div className="navbar bg-base-100">
-                    <div className="navbar-start">
-                        <div className="dropdown">
-                            <label tabIndex={0} className="btn btn-ghost btn-circle">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                            </label>
-                            <ul className={styles.list}>
-                                
-                                <li className={styles.listItem}>
-                                    <Link href="/page1">
-                                        <FaHome />
-                                    </Link>
-                                </li>
-                                <li className={styles.listItem}>
-                                    <Link href="/page2">
-                                        <FaMedapps />
-                                    </Link>
-                                </li>
-                                <li className={styles.listItem}>
-                                    <Link href="/page3">
-                                        <FaGithub />
-                                    </Link>
-                                </li>
-                                <li className={styles.listItem}>
-                                    <Link href="/page4">
-                                        <FaTwitter />
-                                    </Link>
-                                </li>
-                                <li className={styles.listItem}>
-                                    <Link href="/page4">
-                                        <FaLinkedin />
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </nav> */}
         </>
     )
 }
