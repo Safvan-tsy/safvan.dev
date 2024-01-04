379b8fead979b7d7001654bb842517bf0b572819
Safvan-tsy <safvanmanikulath@gmail.com>
2024-01-04 22:11:50 +0530

    in progress

--- app/components/about/Experience.tsx
+++ app/components/about/Experience.tsx
@@ -12,18 +12,43 @@ const Experience = () => {
         <div className='m-2 p-2 flex flex-col md:flex-row border-b-2 border-gray-600'>
           <div className='md:w-[30%]'>
             <div>
-            <h2 className='text-semibold tracking-wider'>
-              Unfold Solution 
-            </h2>
-            <p className='text-sm text-gray-400'>1 yr 1 mo</p>
-            <p className='text-sm text-gray-400'>Remote - Melbourne, Australia</p>
+              <h2 className='text-semibold tracking-wider'>
+                Unfold Solution
+              </h2>
+              <p className='text-sm text-gray-400'>1 yr 1 mo</p>
+              <p className='text-sm text-gray-400'>Remote - Melbourne, Australia</p>
             </div>
-            <h3>
-              January 2023 – Present
-            </h3>
-            <h4 className=''>
-              Software Engineer
-            </h4>
+            <div className='p-2'>
+              <ul>
+                <li>
+                  <div>
+                    <h3 className=''>Software Engineer</h3>
+                    <span className='text-sm text-gray-400'>Dec 2023 - Present</span>
+                  </div>
+                </li>
+                <li className=''>
+                  <div className='relative'>
+                    {/* content: "";
+                    position: absolute;
+                    left: calc(-3.2rem - 2px / 2);
+                    top: calc(calc(1.2rem + 0.8rem) + 0.8rem + 0.8rem);
+                    height: calc(100% - (2 * 1.2rem));
+                    width: 2px;
+                    background-color: var(--color-border-faint); */}
+                    <h3>Junior Software Engineer</h3>
+                    <span className='text-sm text-gray-400'>May 2023 - Nov 2023</span>
+                    <div className="absolute left-[calc(-3.2rem-1px)] top-[calc(calc(1.2rem+0.8rem)+0.8rem+0.8rem)] 
+                  h-[calc(100%-2*(1.2rem))] w-2 bg-gray-400"></div>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <h3>Trainee Developer</h3>
+                    <span className='text-sm text-gray-400'>Jan 2023 - April 2023</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className='md:w-[70%]'>
             <p className='flex flex-wrap text-[--paragraph-font] font-inter tracking-wide text-md 
