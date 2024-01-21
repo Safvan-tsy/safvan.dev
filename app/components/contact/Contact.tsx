e241c1199ad39b8af6a38918e020ef9a215ed2c9
Safvan-tsy <safvanmanikulath@gmail.com>
2024-01-21 18:06:09 +0530

    feat:contact form

--- app/components/contact/Contact.tsx
+++ app/components/contact/Contact.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import { useForm, ValidationError } from "@formspree/react";
+import Loader from "./Loader";
+import Alert from "./Alert";
 
 const title = {
   initial: {
@@ -30,10 +33,41 @@ const word = {
 };
 
 const Contact = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [message, setMeassage] = useState();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [state, handleSubmit] = useForm("mvoezpyj");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
+  const validateForm = () => {
+    if (!name.trim()) return false;
+    if (!email.trim() || !email.includes("@")) return false;
+    if (!message.trim()) return false;
+
+    return true;
+  };
+
+  const removeSuccess = () => {
+    setTimeout(() => {
+      setSuccess(false);
+    }, 4000);
+  };
+
+  const submitMessage = async () => {
+    if (validateForm()) {
+      setError(false);
+      await handleSubmit({ name, email, message }).then(() => {
+        setName("");
+        setEmail("");
+        setMessage("");
+        setSuccess(true);
+        removeSuccess();
+      });
+    } else {
+      setError(true);
+    }
+  };
 
   return (
     <motion.div
@@ -59,12 +93,18 @@ const Contact = () => {
           <input
             placeholder="Name"
             type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="outline-none text-slate-400 rounded-md p-3 bg-[--input-bg]
               my-2 w-full"
           />
           <input
             placeholder="Email"
-            type="text"
+            id="email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="outline-none text-slate-400 rounded-md p-3 bg-[--input-bg]
                 my-2 w-full"
           />
@@ -72,18 +112,33 @@ const Contact = () => {
         <motion.div variants={word} className="">
           <textarea
             placeholder="Message"
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             className="bg-[--input-bg] text-slate-400 h-[10rem] placeholder:text-slate-600 
               placeholder:opacity-50 outline-block p-3 outline-none rounded-md
               my-2 w-full"
           />
         </motion.div>
-        <motion.div variants={word} className="">
-          <button
-            className="cursor-pointer items-center p-2 my-2 rounded-md 
+        <motion.div
+          variants={word}
+          className="flex justify-center flex-col items-center flex-wrap gap-2"
+        >
+          {error && <Alert type="Danger" text="Please provide all fields" />}
+          {state.submitting ? (
+            <Loader />
+          ) : (
+            <button
+              onClick={submitMessage}
+              className="cursor-pointer items-center p-2 my-2 rounded-md 
           w-full active:translate-y-1 bg-gray-500 font-semibold tracking-wide"
-          >
-            Submit
-          </button>
+              disabled={state.submitting}
+            >
+              Submit
+            </button>
+          )}
+          {success && <Alert type="success" text="Thank you for Contacting" />}
         </motion.div>
       </div>
     </motion.div>
