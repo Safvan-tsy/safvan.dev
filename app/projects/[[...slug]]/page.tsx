82016039261af22704b9831838abf57f0b13d6e7
Safvan-tsy <safvanmanikulath@gmail.com>
2024-04-16 21:26:37 +0530

    next not found from next/nav

--- app/projects/[[...slug]]/page.tsx
+++ app/projects/[[...slug]]/page.tsx
@@ -1,8 +1,10 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 const Project: React.FC<{ params: { slug: string } }> = ({ params }) => {
-  if (params.slug?.length == 1) return <div>page for {params.slug[0]}</div>
-  return <div>Project page</div>;
+  if (params.slug?.length == 1) return <div>page for {params.slug[0]}</div>;
+  else if (params.slug?.length == 0) return <div>Project page</div>;
+  else notFound();
 };
 
 export default Project;
