c097c46afa335c0c667e2494497162ce51911734
Safvan-tsy <safvanmanikulath@gmail.com>
2024-05-30 22:21:51 +0530

    feat:all projects card

--- app/projects/data/projects.ts
+++ app/projects/data/projects.ts
@@ -1,10 +1,30 @@
+export interface ProjectType {
+  id: string;
+  slug: string;
+  projectName: string;
+  profile: string;
+  summary: string;
+  description: string;
+}
 export const projects = [
   {
     id: "1",
-    projectName: "sample",
+    slug: "aix-for-career",
+    projectName: "aix - for careers",
+    profile:
+      "https://res.cloudinary.com/safvan/image/upload/v1717081390/Screenshot_from_2024-05-30_20-32-10_knvv1h.png",
+    summary:
+      "I also excel in frontend development incorporating various technologies With strong emphasis on delivering high-quality solutions. My expertise spans both backend and frontend realms, With over 1+ years of experience.",
+    description: "",
   },
   {
     id: "2",
-    projectName: "sample",
+    slug: "blogx-blogging",
+    projectName: "blogx blogging",
+    profile:
+      "https://res.cloudinary.com/safvan/image/upload/v1717081390/Screenshot_from_2024-05-30_20-32-10_knvv1h.png",
+    summary:
+      "I also excel in frontend development incorporating various technologies With strong emphasis on delivering high-quality solutions. My expertise spans both backend and frontend realms, With over 1+ years of experience.",
+    description: "",
   },
 ];
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,10 +5,12 @@ import ProjectListCard from "../components/projects/ProjectListCard";
 const Page = async () => {
   const projectList = projects;
   return (
-    <section className="flex md:w-[90%] w-full justify-center flex-wrap items-center
-     mb-4 p-3 lg:p-6 lg:pb-8 xl:p-10 gap-14">
+    <section
+      className="flex md:w-[90%] w-full justify-center flex-wrap items-center
+     mb-4 p-3 lg:p-6 lg:pb-8 xl:p-10 gap-14"
+    >
       {projectList.map((project) => (
-        <ProjectListCard key={project.id} />
+        <ProjectListCard key={project.id} project={project} />
       ))}
     </section>
   );
