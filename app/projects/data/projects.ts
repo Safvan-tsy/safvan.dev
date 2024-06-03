4297fb1d49cf8610405222cf113748201ddd379a
Safvan-tsy <safvanmanikulath@gmail.com>
2024-06-03 21:33:32 +0530

    feat:add aix project

--- app/projects/data/projects.ts
+++ app/projects/data/projects.ts
@@ -4,15 +4,15 @@ export interface ProjectType {
   projectName: string;
   profile: string;
   summary: string;
-  techStack?: {
+  techStack: {
     tech: string;
     values: string[];
   }[];
-  feat?: string[];
+  feat: string[];
   visuals: string[];
   repoLink: string;
-  challenges?: { challenge: string; solution: string }[];
-  learningOutcome?: string[];
+  challenges: { challenge: string; solution: string }[];
+  learningOutcome: string[];
 }
 export const projects: ProjectType[] = [
   {
@@ -27,7 +27,7 @@ export const projects: ProjectType[] = [
       { tech: "Languages", values: ["JavaScript", "TypeScript"] },
       {
         tech: "Frameworks/Libraries",
-        values: ["Next.js 14", "Tailwind CSS", "DaisyUI"],
+        values: ["Next.js 14", "Tailwind CSS", "DaisyUI", "NextAuth.js"],
       },
       { tech: "Database", values: ["PostgreSQL"] },
       { tech: "ORM", values: ["Prisma"] },
@@ -41,6 +41,7 @@ export const projects: ProjectType[] = [
       "Single admin capability",
       "Dark and light theme support",
       "Open-source with easy self-hosting",
+      "Utilizes NextAuth for secure user authentication.",
     ],
     repoLink: "https://github.com/Safvan-tsy/blogx",
     visuals: [
@@ -66,20 +67,60 @@ export const projects: ProjectType[] = [
       },
     ],
     learningOutcome: [
-      "Gained hands-on experience with Next.js 14 and its new features.",
+      "Gained experience with managing a monorepo setup using Turbo Repo for streamlined development and deployment processes.",
       "Improved understanding of Prisma for database management.",
       "Enhanced skills in responsive web design using Tailwind CSS and DaisyUI.",
     ],
   },
-  // {
-  //   id: 2,
-  //   slug: "aix-for-career",
-  //   projectName: "aix - for careers",
-  //   profile:
-  //     "https://res.cloudinary.com/safvan/image/upload/v1717081390/Screenshot_from_2024-05-30_20-32-10_knvv1h.png",
-  //   summary:
-  //     "I also excel in frontend development incorporating various technologies With strong emphasis on delivering high-quality solutions. My expertise spans both backend and frontend realms, With over 1+ years of experience.",
-  //   repoLink: "",
-  //   visuals: [],
-  // },
+  {
+    id: 2,
+    slug: "ai-for-job-seekers",
+    projectName: "AIx: AI-Powered Resume and Job Search Optimization Platform",
+    profile:
+      "https://res.cloudinary.com/safvan/image/upload/v1717429077/Screenshot_from_2024-06-03_21-02-57_upmamg.png",
+    summary:
+      "Aix is a web application developed for the Instill AI Hackathon, designed to assist job seekers by generating optimized resumes and Boolean search texts for advanced job searches. Users can input their basic details, experience, education, and skills into the platform. Aix leverages AI pipelines and GPT-3.5 within Instill AI's pipeline builder to process and refine this data, making it ATS-friendly and generating the required documents.",
+    techStack: [
+      { tech: "Languages", values: ["JavaScript", "TypeScript"] },
+      {
+        tech: "Frameworks/Libraries",
+        values: ["Next.js 14", "Tailwind CSS", "shadcn", "Express.js"],
+      },
+      {
+        tech: "AI and Automation",
+        values: ["Instill AI", "GPT-3.5", "Puppeteer"],
+      },
+      { tech: "Monorepo Tooling", values: ["Turbo Repo"] },
+    ],
+    feat: [
+      "User input of basic details, experience, education, and skills",
+      "AI-driven resume generation",
+      "AI-generated Boolean search texts for advanced job searching",
+      "Instill AI pipeline for data optimization and ATS-friendly resume creation",
+      "Backend endpoint to refactor data into a PDF resume",
+      "Instill AI pipeline to generate Boolean search texts from user data",
+    ],
+    repoLink: "https://github.com/Safvan-tsy/aix",
+    visuals: [
+      "https://res.cloudinary.com/safvan/image/upload/v1717429077/Screenshot_from_2024-06-03_21-02-57_upmamg.png",
+    ],
+    challenges: [
+      {
+        challenge:
+          "Integrating AI models and ensuring accurate data processing.",
+        solution:
+          "Developed and fine-tuned two Instill AI pipelines using GPT-3.5 within the pipeline builder for data optimization and document generation.",
+      },
+      {
+        challenge: "Generating ATS-friendly resumes automatically",
+        solution:
+          "Implemented an AI pipeline to format user data into an ATS-compliant resume, processed via Puppeteer for PDF conversion.",
+      },
+    ],
+    learningOutcome: [
+      "Enhanced proficiency with Next.js 14 and modern frontend development practices.",
+      "Deepened understanding of integrating AI models into web applications using GPT-3.5 and Instill AI.",
+      "Improved backend skills with Express.js and automated document generation with Puppeteer.",
+    ],
+  },
 ];
